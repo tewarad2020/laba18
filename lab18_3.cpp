7c2f395f5521b900765c40c1a65364124abf8e8e
F_ford <76035588+tewarad2020@users.noreply.github.com>
2021-02-23 17:44:58 +0700

    Update lab18_3.cpp

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -8,9 +8,10 @@
 using namespace std;
 
 struct student{
-
-    //Define struct student with four member (name ,id , gender, gpa);
-    
+    string name; 
+    int id; 
+    char gender[2];
+    float gpa;
 };
 
 struct course{
@@ -20,7 +21,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ 
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -51,7 +52,7 @@ void printreport(vector<course> allcourses){
 		
 	}
 	cout << "-----------------------------------------------------------------------------\n";
-}
+} 
 
 int main(){
 	ifstream student_file("students.txt");
@@ -62,14 +63,16 @@ int main(){
 	string textline;
 	
 	while(getline(student_file,textline)){
-		student s; 
-		
-		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-
+		student s;
+		char name[100]; 
+		char format[] = "%[^,],%d,%[^,],%f";
+        sscanf(textline.c_str(),format,name,&s.id,&s.gender,&s.gpa);
+		s.name = name;
 		allstudents.push_back(s); 		
 	}
 	
 	int state = 1;
+	int n = 0;
 	while(getline(course_file,textline)){
 		if(state == 1){
 			course c;
@@ -83,18 +86,15 @@ int main(){
 			if(textline == "> Students"){
 				state = 3;
 			}else{
-			
-			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
-			    
+			    allcourses[n].lecture_list.push_back(textline);
 			}			
 		}else{
 			if(textline == "---------------------------------------"){
 				state = 1;
+				n++;
 			}else{
-				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
-				//Append (push_back) p to student_list of the recently added course in allcourses[];
-				
+				student *p = findstudent(allstudents,atoi(textline.c_str()));
+				allcourses[n].student_list.push_back(p);
 			}
 		}
 	}
