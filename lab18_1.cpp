624b880b3f1248e502891fdd533c2dbd275b29aa
F_ford <76035588+tewarad2020@users.noreply.github.com>
2021-02-23 17:43:53 +0700

    Update lab18_1.cpp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
+#include<algorithm>
+
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double  overlap(Rect R1, Rect R2){
+    double mx_x, mx_y, mn_x, mn_y;
+    mx_x = min(R1.x+R1.w,R2.x+R2.w);
+    mn_x = max(R1.x,R2.x);
+    mx_y = max(R1.y-R1.h,R2.y-R2.h);
+    mn_y = min(R1.y,R2.y);
+    if ((mx_x-mn_x)*(mn_y-mx_y) > 0) return ((mx_x-mn_x)*(mn_y-mx_y));
+	else return 0;
 }
